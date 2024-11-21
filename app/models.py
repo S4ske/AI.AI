3b92d20822154c22651e9eca1a047e769b503373
S4ske <sany1111.ru@mail.ru>
2024-11-21 11:45:52 +0500

    Добавил модели Project и Animation, дополнил модель User

--- app/models.py
+++ app/models.py
@@ -1,18 +1,66 @@
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
-from sqlalchemy import String, Boolean, DateTime
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
+from sqlalchemy import String, Boolean, DateTime, Integer, ForeignKey, event, Float
 from uuid import uuid4, UUID
+from datetime import datetime, UTC
 
 Base = declarative_base()
 
 
-class User(Base):
-    __tablename__ = "user"
+def datetime_now_utc(_):
+    return datetime.now(UTC)
 
-    id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
-    email = mapped_column(String(255), unique=True, index=True)
-    username = mapped_column(String(length=255), default=None, nullable=True)
-    hashed_password = mapped_column(String)
-    is_active = mapped_column(Boolean, default=False)
-    is_superuser = mapped_column(Boolean, default=False)
-    created_at = mapped_column(DateTime)
-    updated_at = mapped_column(DateTime)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(255), unique=True, index=True, nullable=False
+    )
+    username: Mapped[str | None] = mapped_column(
+        String(length=255), default=None, nullable=True
+    )
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime_now_utc, nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime_now_utc, nullable=False
+    )
+
+    projects: Mapped[list["Project"]] = relationship(back_populates="owner")
+
+
+@event.listens_for(User, "before_update")
+def set_updated_at(mapper, connection, target):
+    target.updated_at = datetime.now(UTC)
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    owner_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"), nullable=False)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    height: Mapped[int] = mapped_column(Integer, nullable=False, default=300)
+    width: Mapped[int] = mapped_column(Integer, nullable=False, default=300)
+    images_paths: Mapped[list[str]] = mapped_column(String, nullable=False, default=[])
+    fps: Mapped[int] = mapped_column(Integer, nullable=False, default=20)
+    duration: Mapped[float] = mapped_column(Float, nullable=False)
+
+    animations: Mapped[list["Animation"]] = relationship(back_populates="project")
+    owner: Mapped[User] = relationship(back_populates="projects")
+
+
+class Animation(Base):
+    __tablename__ = "animations"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    param_name: Mapped[str] = mapped_column(String, nullable=False)
+    start_frame: Mapped[int] = mapped_column(Integer, nullable=False)
+    end_frame: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    project_id: Mapped[int] = mapped_column(ForeignKey("projects.id"), nullable=False)
+    project: Mapped[Project] = relationship(back_populates="animations")
