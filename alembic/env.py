e0799b4b2e49814009442e16a6102ed632a00572
S4ske <sany1111.ru@mail.ru>
2024-11-25 15:33:04 +0500

    Поменял url бд на psycopg2

--- alembic/env.py
+++ alembic/env.py
@@ -13,7 +13,7 @@ from app.core.config import settings
 config = context.config
 
 section = config.config_ini_section
-config.set_section_option(section, "POSTGRES_URL", str(settings.POSTGRES_URL_ASYNC))
+config.set_section_option(section, "POSTGRES_URL", str(settings.POSTGRES_URL))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
